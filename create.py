8b37e733fda1c23666c78c42425de6b83ffb9497
lionkingofnorth <luoyang19922@gmail.com>
2020-03-11 21:53:51 +0800

    Init

---
+++ create.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# We create a 1D ndarray that contains only integers
+x = np.array([1, 2, 3, 4, 5])
+
+# We print x
+print()
+print('x = ', x)
+print()
+
+# We print information about x
+print('x has dimensions:', x.shape)
+print('x is an object of type:', type(x))
+print('The elements in x are of type:', x.dtype)
